dd71567e03b7a12387479988821cdac87a53b0be
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-04-11 14:38:30 -0300

    Change the backend port to 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
