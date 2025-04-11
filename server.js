3a3d4c6ac7a61b43adde2ef1b52ae534675e230a
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-04-11 13:55:16 -0300

    Fix the port of JSON Server

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => {
-    console.log(`JSON Server rodando em http://localhost:${port}`);
+    console.log(`JSON Server rodando na porta ${port}`);
 });
