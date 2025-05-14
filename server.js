d07d01de7d281f7b3066db9c771c16b072598e83
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-05-14 15:01:17 -0300

    add CORS fix

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8080;
 
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 server.use(middlewares);
 server.use(router);
 
