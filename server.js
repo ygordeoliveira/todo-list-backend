623faea185502aa0cf3da46a57f03af089815227
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-05-14 15:26:44 -0300

    Enable CORS inside the file

--- server.js
+++ server.js
@@ -2,16 +2,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 
-
-server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); 
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
-
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
